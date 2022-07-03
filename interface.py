1e1eaeebd8e7ecc09595f7c955ea57b55dc1ca02
Mad_cyber <66136672+madhusudhanpatnaik@users.noreply.github.com>
2022-07-03 14:42:45 +0530

    Add files via upload

--- interface.py
+++ interface.py
@@ -1,13 +1,29 @@
+from cgitb import text
 from tkinter import *
+from atexit import register
+from typing import get_args
+from unicodedata import name
+from cryptography.fernet import Fernet
+import re
+import ctypes
+import time
+import os
+import sys
+from cred_view import *
 
 
 ws = Tk()
-ws.title('PythonGuides')
+ws.title('CIPHER NOTES')
 ws.config(bg='#0B5A81')
+
+name_var2 = StringVar()
+password_var2 = StringVar()
 
 f = ('Times', 14)
 var = StringVar()
 var.set('male')
+name_var1 = ""
+password_var1 = ""
 
 right_frame = Frame(
     ws,
@@ -34,11 +50,13 @@ Label(
 
 register_name = Entry(
     right_frame,
+    textvariable=name_var2,
     font=f
 )
 
 register_pwd = Entry(
     right_frame,
+    textvariable=password_var2,
     font=f,
     show='*'
 )
@@ -60,18 +78,26 @@ login_btn = Button(
     font=f,
     relief=SOLID,
     cursor='hand2',
-    command=lambda: callback_function2())
+    command=lambda: callback_function2(name_var2, password_var2))
 
 
 def callback_function():
     print("register")
-    import credsavig
-    # do something
+    ws.destroy()
+    import credsavig as cred
+    cred.main()
 
 
-def callback_function2():
+def callback_function2(name_var2, password_var2):
+    name2 = name_var2.get()
+    password_var2 = password_var2.get()
+    if (name2 == user and password_var2 == passwd):
+        print("useername")
+    else:
+        print("login not wokring")
+
     print("login")
-    # do something
+    #do something
 
 
 register_name.grid(row=0, column=1, pady=10, padx=20)
